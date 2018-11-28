cecb2abe4169287b1054660911f1151dbc571c4a
Aselium <dominic_t@hotmail.co.uk>
2018-11-28 19:13:45 +0000

    Download Code

---
+++ app/src/main/java/aselia/com/coinz/DownloadCompleteRunner.java
@@ -0,0 +1,11 @@
+package aselia.com.coinz;
+
+public class DownloadCompleteRunner{
+
+    static String result;
+
+    public static void downloadComplete(String result){
+        DownloadCompleteRunner.result = result;
+    }
+}
+
